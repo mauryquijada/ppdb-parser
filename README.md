66e749e4ffdb7c2b3fb1cdd6b965fabe0f6fdf23
Maury Quijada <maury.quijada@gmail.com>
2015-11-05 17:24:20 -0800

    Add support for parallelized reading and writing to minimize memory usage. :D.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ppdb-parser
-A Python-based tool that parses gzipped text database from the PPDB website (http://www.cis.upenn.edu/~ccb/ppdb/), and returns objects that represent database rows.
+A Python-based tool that parses text files from the PPDB website
+(http://www.cis.upenn.edu/~ccb/ppdb/), and writes the information to a
+file that Moses (an open-source statistical machine translation tool)
+can read. Note that this tool expects text files to have lines of the
+form "LHS ||| SOURCE ||| TARGET ||| (FEATURE=VALUE )* ||| ALIGNMENT".
+
+Furthermore, to parallelize processing, the tool also expects the
+input text file to be sorted by TARGET.
--- parser.py
+++ parser.py
@@ -1,4 +1,6 @@
 import argparse
+import collections
+import itertools
 import math
 import re
 import sys
@@ -6,139 +8,179 @@ import time
 
 from multiprocessing import Pool
 
-PPDB_DELIMITER = " ||| "
+PPDB_COLUMN_DELIMITER = " ||| "
+PHRASE_TABLE_COLUMN_DELIMITER = " ||| " 
 POS_MAPPING_REGEX = r'\[[^\]]+\]\s*'
-LOG_BASE = 2
+LOG_BASE = math.e
+COUNTER = 0
+PROCESSED= 0
+
+class Paraphrase:
+	def __init__(self):
+		self.lhs = None
+		self.source = None
+		self.target = None
+		self.features = None
+		self.alignment = None
 
 def get_file_chunks(file_name, size=1024 ** 2):
+	global COUNTER
 	with open(file_name) as f:
 		f.seek(0, 1)
 		while True:
-			start = f.tell()
+			start_byte = f.tell()
 			f.seek(size, 1)
 			# Make sure that we have the entirety of the last line.
 			s = f.readline()
+			bytes_to_read = f.tell() - start_byte
+			end_of_last_line = f.tell()
 
-			yield file_name, start, f.tell() - start
+			f.seek(start_byte)
+			lines = f.read(bytes_to_read).splitlines()
+
+			last_line_target_word = get_ppdb_line_target(lines[-1].split(PPDB_COLUMN_DELIMITER))
+
+			# This is where the sorting comes in. Since we want to have all source
+			# phrases for a given target word in the same chunk (so that normalization
+			# happens properly) we need to keep reading lines until we find a new
+			# target word.
+			next_line = f.readline()
+			if not next_line:
+				COUNTER += len(lines)
+				yield lines
+				break
+
+			next_line_target_word = get_ppdb_line_target(next_line.split(PPDB_COLUMN_DELIMITER))
+			while last_line_target_word == next_line_target_word:
+				lines.append(next_line)
+				end_of_last_line = f.tell()
+				next_line = f.readline()
+				next_line_target_word = get_ppdb_line_target(next_line.split(PPDB_COLUMN_DELIMITER))
+
+			f.seek(end_of_last_line, 0)
+			COUNTER += len(lines)
+			yield lines
 			if not s:
 				break
 
-def process_file_chunk(input):
-	file_name, begin_byte, end_byte = input
+def process_file_chunk(lines):
+	paraphrase_probabilities = collections.defaultdict(dict)
 
-	with open(file_name) as f:
-		f.seek(begin_byte)
-		paraphrase_probabilities = {}
-		for line in f.read(end_byte).splitlines():
-			paraphrase = process_ppdb_line(line)
-			source_paraphrase = paraphrase["source"]
-			target_paraphrase = paraphrase["target"]
+	for line in lines:
+		paraphrase = process_ppdb_line(line)
+		source_paraphrase = paraphrase.source
+		target_paraphrase = paraphrase.target
 
-			if target_paraphrase not in paraphrase_probabilities:
-				paraphrase_probabilities[target_paraphrase] = {}
+		probability = float(paraphrase.features["p(f|e)"])
+		paraphrase_probabilities[target_paraphrase][source_paraphrase] =\
+		    LOG_BASE ** (-1 * probability)
 
-			probability = float(paraphrase["features"]["p(f|e)"])
-			paraphrase_probabilities[target_paraphrase][source_paraphrase] =\
-			    LOG_BASE ** (-1 * probability)
+	for target_phrase, source_phrases in paraphrase_probabilities.iteritems():
+		paraphrase_probabilities[target_phrase] =\
+	 		normalize_phrase_probabilities(source_phrases)
 
-		return paraphrase_probabilities
+	return paraphrase_probabilities
 
 """
 Given a line in a PPDB file, process it appropriately and return a dictionary
 of all the features of interest.
 """
 def process_ppdb_line(line):
-	line_elements = line.split(PPDB_DELIMITER)
-	paraphrase = {}
+	line_elements = line.split(PPDB_COLUMN_DELIMITER)
+	paraphrase = Paraphrase()
 
-	paraphrase["lhs"] = line_elements[0]
-	paraphrase["source"] = re.sub(POS_MAPPING_REGEX, r'', line_elements[1]).strip()
-	paraphrase["target"] = re.sub(POS_MAPPING_REGEX, r'', line_elements[2]).strip()
-
-	features = line_elements[3].split(' ')
-	feature_dict = {}
-	for feature in features:
-		key, value = feature.split('=')
-		feature_dict[key] = value
-	paraphrase["features"] = feature_dict
-
-	paraphrase["alignment"] = line_elements[4]
+	paraphrase.lhs = get_ppdb_line_lhs(line_elements)
+	paraphrase.source = get_ppdb_line_source(line_elements)
+	paraphrase.target = get_ppdb_line_target(line_elements)
+	paraphrase.features = get_ppdb_line_features(line_elements)
+	paraphrase.alignment = get_ppdb_line_alignment(line_elements)
 
 	return paraphrase
 
 """
-Given a list of dictionaries of the form {target_paraphrase:
-{possible_source_paraphrase: 0.xxx}, ...}, merge each dictionary together into one
-large dictionary.
+Given a PPDB line properly split, return the LHS.
 """
-def merge_paraphrase_probabilities(list_paraphrase_probs):
-	all_paraphrases_probs = {}
-	for paraphrase_probs in list_paraphrase_probs:
-		for target_paraphrase, source_paraphrases in paraphrase_probs.iteritems():
-			if target_paraphrase in all_paraphrases_probs:
-				all_paraphrases_probs[target_paraphrase].update(source_paraphrases)
-			else:
-				all_paraphrases_probs[target_paraphrase] = source_paraphrases
-	return all_paraphrases_probs
+def get_ppdb_line_lhs(split_line):
+	return split_line[0]
 
 """
-Given a dictionary of {possible_source_paraphrase: 0.xxx, }, normalize the probabilities.
+Given a PPDB line properly split, return the source.
 """
-def normalize_phrase_probabilities(target_dict_entry):
-	target, possible_source_paraphrase = target_dict_entry
+def get_ppdb_line_source(split_line):
+	return re.sub(POS_MAPPING_REGEX, r'', split_line[1]).strip()
+
+"""
+Given a PPDB line properly split, return the target.
+"""
+def get_ppdb_line_target(split_line):
+	return re.sub(POS_MAPPING_REGEX, r'', split_line[2]).strip()
+
+"""
+Given a PPDB line properly split, return the features.
+"""
+def get_ppdb_line_features(split_line):
+	features = split_line[3].split(' ')
+	feature_dict = {}
+	for feature in features:
+		key, value = feature.split('=')
+		feature_dict[key] = value
+
+	return feature_dict
+
+"""
+Given a PPDB line properly split, return the alignment.
+"""
+def get_ppdb_line_alignment(split_line):
+	return split_line[4]
+
+"""
+Given a dictionary of {source_phrase_1: 0.xxx, source_phrase_2: 0.xxx, ...},
+normalize the probabilities.
+"""
+def normalize_phrase_probabilities(source_phrases):
 	probability_sum = 0
 
-	for _, source_probability in possible_source_paraphrase.iteritems():
-		probability_sum += source_probability
+	for _, probability in source_phrases.iteritems():
+		probability_sum += probability
 
-	for source, _ in possible_source_paraphrase.iteritems():
-		possible_source_paraphrase[source] /= probability_sum * 1.0
+	for source_phrase, _ in source_phrases.iteritems():
+		source_phrases[source_phrase] /= probability_sum * 1.0
 
-	return {target: possible_source_paraphrase}
+	return source_phrases
 
-def main(ppdb_filename):
+def create_phrase_table_line(target_phrase, source_phrase, probability):
+	return source_phrase + PHRASE_TABLE_COLUMN_DELIMITER +\
+			target_phrase + PHRASE_TABLE_COLUMN_DELIMITER +\
+			str(probability) + PHRASE_TABLE_COLUMN_DELIMITER +\
+			PHRASE_TABLE_COLUMN_DELIMITER.strip() + "\n"
+
+def main(ppdb_filename, output_filename):
+	global PROCESSED
 	process_pool = Pool()
-	results = process_pool.map_async(process_file_chunk, get_file_chunks(ppdb_filename))
+
+	# Create a new iterator wrapper that passes along the producer queue with lines
+	# to process_file_chunk.
+	results = process_pool.imap(process_file_chunk,
+		get_file_chunks(ppdb_filename), chunksize=1)
 	process_pool.close()
 
-	print "Extracting paraphrases into a table in memory..."
-	while not results.ready():
-		remaining = results._number_left
-		sys.stdout.write("\r")
-		print "Waiting for", remaining, "paraphrase extraction tasks to complete..."
-		time.sleep(5)
+	with open(output_filename, "w") as output:
+		for result in results:
+			for target_phrase, source_phrases in result.iteritems():
+				for source_phrase, probability in source_phrases.iteritems():
+						PROCESSED += 1
+						output.write(create_phrase_table_line(target_phrase,
+							source_phrase, probability))
 
-	all_paraphrases = merge_paraphrase_probabilities(results.get())
-	print "Extracted paraphrases into a table in memory."
-
-	process_pool = Pool()
-	results = process_pool.map_async(normalize_phrase_probabilities, all_paraphrases.items())
-	process_pool.close()
-
-	print "Normalizing paraphrase probabilities..."
-	while not results.ready():
-		remaining = results._number_left
-		sys.stdout.write("\r")
-		print "Waiting for", remaining, "paraphrase normalization tasks to complete..."
-		time.sleep(5)
-
-	all_paraphrases = merge_paraphrase_probabilities(results.get())
-	print "Normalized paraphrase probabilities."
-
-	# Write to file.
-	print "Writing paraphrases to file..."
-	with open("output.txt", "w") as new_phrasetable_file:
-		for target, sources in all_paraphrases.iteritems():
-			for source, source_probability in sources.iteritems():
-				new_phrasetable_file.write(source + PPDB_DELIMITER +\
-					target + PPDB_DELIMITER +\
-					str(source_probability) + PPDB_DELIMITER +\
-				"|||\n")
+	print "lines are ", COUNTER
+	print "processed are ", PROCESSED
 
 if __name__ == "__main__":
 	ppdb_filename = None
 	parser = argparse.ArgumentParser(description="Parses a given PPDB file into a Moses phrase table.")
-	parser.add_argument("ppdb_filename")
+	parser.add_argument("ppdb_filename", help="Filepath to PPDB database. Note that this tool expects" +
+		" the file to be sorted by TARGET field.")
+	parser.add_argument("output_filename", help="Output phrase table filename.")
 
 	args = parser.parse_args()
-	main(args.ppdb_filename)
+	main(args.ppdb_filename, args.output_filename)
